87610f4a6f7f2bcbf8a4c71d42880c40119e289e
帅逼 <281148314@qq.com>
2022-01-27 01:20:13 +0800

    消费者客户端连接-优化重连检查

--- conn/consumer.go
+++ conn/consumer.go
@@ -18,19 +18,24 @@ var receiveChan = make(chan model.Message)
 
 // NewConsumerConn 创建一个消费者连接
 func NewConsumerConn(consumer conf.Consumer) error {
+
 	wsUrl := fmt.Sprintf("%s://%s:%s%s%s/%s", consumer.MqProtocol, consumer.MqAddr, consumer.MqPort, "/Consumers/Conn/", consumer.Topic, consumer.ConsumerId)
 	client, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
 	if err != nil {
 		return err
 	}
 
+	//将websocket连接添加到消费者连接列表
 	consumerConn[client] = wsUrl
+	//开启连接协程
 	go consumerReceiveHandle(consumer.SecretKey, client)
 
+	//开启连接检查协程
 	go func() {
 		for {
 			checkConsumer(consumer)
-			time.Sleep(time.Second * time.Duration(3))
+			fmt.Printf("\033[1;32;40m%s\033[0m\n", "check consumer")
+			time.Sleep(time.Second * time.Duration(consumer.CheckTime))
 		}
 	}()
 	return nil
@@ -49,6 +54,7 @@ func consumerReceiveHandle(secretKey string, client *websocket.Conn) {
 
 	//验证密钥
 	for {
+		//读取消息队列发送过来的提示
 		messageType, message, err := client.ReadMessage()
 		if err != nil {
 			log.Fatal(err)
@@ -59,8 +65,10 @@ func consumerReceiveHandle(secretKey string, client *websocket.Conn) {
 			return
 		}
 
+		//请输入访问密钥
 		if string(message) == "Please enter the secret key" {
 
+			//发送密钥
 			err = client.WriteMessage(1, []byte(secretKey))
 			if err != nil {
 				log.Fatal(err)
@@ -68,9 +76,13 @@ func consumerReceiveHandle(secretKey string, client *websocket.Conn) {
 			}
 		}
 
+		//访问密钥错误
 		if string(message) == "Secret key matching error" {
+			log.Fatal("Secret key matching error")
 			continue
 		}
+
+		//访问密钥正确
 		if string(message) == "Secret key matching succeeded" {
 			break
 		}
@@ -104,9 +116,9 @@ func checkConsumer(consumer conf.Consumer) {
 	flag := true
 	wsUrl := fmt.Sprintf("%s://%s:%s%s%s/%s", consumer.MqProtocol, consumer.MqAddr, consumer.MqPort, "/Consumers/Conn/", consumer.Topic, consumer.ConsumerId)
 
-	//检查该连接是否断开
+	//遍历websocket连接列表
 	for _, v := range consumerConn {
-		//如果该连接未断开
+		//如果找到该连接，则表明该连接未断开
 		if wsUrl == v {
 			//跳过
 			flag = false
@@ -118,7 +130,7 @@ func checkConsumer(consumer conf.Consumer) {
 		return
 	}
 
-	//如果该连接已断开，则重新建立连接
+	//如果连接列表中找不到该连接，则表明该连接已断开，则重新建立连接
 	client, _, err := websocket.DefaultDialer.Dial(wsUrl, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -130,10 +142,10 @@ func checkConsumer(consumer conf.Consumer) {
 }
 
 // ConsumerReceive 接收消息
-func ConsumerReceive() (model.Message, bool) {
+func ConsumerReceive() model.Message {
 	//读取receiveChan通道中的消息并返回
-	message, isOk := <-receiveChan
+	message := <-receiveChan
 	message.Status = 1
 	message.ConsumedTime = utils.GetLocalDateTimestamp()
-	return message, isOk
+	return message
 }
