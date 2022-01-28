a779743f3697bf5ba0eb2847ff7fb4a009f2233b
帅逼 <281148314@qq.com>
2022-01-28 16:34:46 +0800

    模板更新

--- model/model.go
+++ model/model.go
@@ -14,12 +14,12 @@ type Message struct {
 	CreateTime   int64  //消息创建时间
 	DelayTime    int64  //延迟推送时间
 	ConsumedTime int64  //消息被消费时间
-	Status       int    //消息状态（-1：刚进入队列,无状态。0：消费失败。1：已被消费）
+	Status       int    //消息状态（-1：未消费。0：消费失败。1：已消费）
 }
 
 // Node 消息队列服务节点结构体
 type Node struct {
-	Name string
-	Addr string
-	Port string
+	Name string //节点名称
+	Addr string //节点ip
+	Port string //节点http服务端口号
 }
